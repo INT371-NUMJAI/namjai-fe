5404c8ea5e8ed86db123db8f919a57741b6e7fa4
fah <supanan.c@mail.kmutt.ac.th>
2022-05-01 00:12:23 +0700

    add NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,17 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
-</script>
-
 <template>
 	<w-app>
-		<header>
-			<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-			<w-icon xl color="primary"> fa fa-star </w-icon>
-			<w-button class="ml-24" bg-color="success">success</w-button>
-			<w-button class="ml-24" bg-color="error">error</w-button>
-
-			<div class="bg-slate-500">
-				<h1 class="text-3xl underline text-namjaigreen font-ibm-plex-thai">สวัสดี ฟอนต์ ไอบีเอ็มใหม่!</h1>
-				<br />
-
-				<h1 class="text-3xl underline text-green-500 font-ibm-plex-thai">HelloWorld</h1>
-				<h1 class="text-3xl underline text-green-500">HelloWorld</h1>
-
-				<HelloWorld msg="You did it!" />
-
-				<nav>
-					<RouterLink to="/">Home</RouterLink>
-					<RouterLink to="/about">About</RouterLink>
-				</nav>
-			</div>
-			<!-- <h1 class="text-3xl underline text-green-500 font-ibmPlex">Hello world!</h1>
-
-		<h1 class="text-3xl underline text-green-500">Hello world!</h1> -->
-		</header>
-
-		<RouterView />
+		<div>
+			<nav-bar />
+		</div>
 	</w-app>
 </template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+
+export default {
+	components: {
+		"nav-bar": NavBar,
+	}
+};
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+	<w-app>
+		<nav class="bg-namjaigreen flex items-center justify-between px-7 py-5 md:py-5 md:px-11 drop-shadow-md">
+			<div class="flex items-center text-white">
+				<img class="w-28" src="../assets/namjai.png" />
+			</div>
+			<div class="md:hidden">
+				<w-icon color="white">fa fa-bars</w-icon>
+			</div>
+			<div class="hidden md:flex md:space-x-11 items-center text-white md:font-semibold">
+				<w-button bg-color="transparent" class="p-4 border-2">มูลนิธิ</w-button>
+				<w-button bg-color="transparent" class="p-4 border-2">โครงการ</w-button>
+				<w-button bg-color="transparent" class="p-4 border-2">จิตอาสา</w-button>
+				<w-button bg-color="transparent" class="p-4 border-2">เกี่ยวกับเรา</w-button>
+				<w-button bg-color="transparent" round outline class="p-4 border-2">เข้าสู่ระบบ</w-button>
+			</div>
+		</nav>
+	</w-app>
+</template>
