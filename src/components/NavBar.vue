100d755651cb290d9b82fc892e2b89fb536e5a24
fah <supanan.c@mail.kmutt.ac.th>
2022-05-05 19:57:39 +0700

    edit component NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,19 +1,19 @@
 <template>
 	<w-app>
-		<nav class="bg-namjaigreen flex items-center justify-between px-7 py-3 md:py-3 md:px-11 drop-shadow-md">
-			<div class="flex items-center text-white">
-				<img class="w-28" src="../assets/namjai.png" />
-			</div>
-			<div class="md:hidden">
-				<w-icon color="white">fa fa-bars</w-icon>
-			</div>
-			<div class="hidden md:flex md:space-x-11 items-center text-white font-semibold">
-				<w-button bg-color="transparent" class="font-medium">มูลนิธิ</w-button>
-				<w-button bg-color="transparent" class="font-medium">โครงการ</w-button>
-				<w-button bg-color="transparent" class="font-medium">จิตอาสา</w-button>
-				<w-button bg-color="transparent" class="font-medium">เกี่ยวกับเรา</w-button>
-				<w-button bg-color="transparent" round outline class="py-4 bd4 font-medium">เข้าสู่ระบบ</w-button>
-			</div>
-		</nav>
+		<nav class="bg-namjaigreen flex items-center justify-between px-7 py-3 lg:py-3 lg:px-11 drop-shadow-md lg:fixed lg:w-screen">
+            <div class="flex items-center text-white">
+                <img class="w-28" src="../assets/namjai.png" />
+            </div>
+            <div class="lg:hidden">
+                <w-icon color="white">fa fa-bars</w-icon>
+            </div>
+            <div class="hidden lg:flex lg:space-x-11 items-center text-white font-semibold">
+                <w-button bg-color="transparent" class="font-medium">มูลนิธิ</w-button>
+                <w-button bg-color="transparent" class="font-medium">โครงการ</w-button>
+                <w-button bg-color="transparent" class="font-medium">จิตอาสา</w-button>
+                <w-button bg-color="transparent" class="font-medium">เกี่ยวกับเรา</w-button>
+                <w-button bg-color="transparent" round outline class="py-4 font-medium">เข้าสู่ระบบ</w-button>
+            </div>
+        </nav>
 	</w-app>
 </template>
