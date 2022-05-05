39b17206b222518cb3e786c062e3caf9a59f8c0f
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2022-05-05 14:56:48 +0700

    [EDIT] tablet responsive in signupForm

--- src/App.vue
+++ src/App.vue
@@ -22,17 +22,17 @@ import HelloWorld from "@/components/HelloWorld.vue";
 				<w-button bg-color="transparent" round outline class="py-4 bd4 font-medium">เข้าสู่ระบบ</w-button>
 			</div>
 		</nav>
-		<div class="h-56 lg:hidden">
+		<div class="h-56 md:h-72 lg:hidden">
 			<div class="w-32 h-32 bg-namjaired rounded-full -ml-10 -mt-5"></div>
 			<div class="w-32 h-32 border-2 border-namjaiyellow rounded-full"></div>
 		</div>
-		<div class="h-screen w-full bg-namjaiwhite rounded-2xl lg:bg-transparent">
-			<div class="mx-8 lg:mx-44">
-				<div class="inline-flex space-x-9">
+		<div class="h-full w-full md:h-[850px] bg-namjaiwhite rounded-2xl lg:bg-transparent">
+			<div class="mx-8 mb-20 md:mx-20 lg:mx-44">
+				<div class="inline-flex space-x-9 md:space-x-[380px]">
 					<span class="mt-10 text-2xl lg:text-[48px] lg:mt-16 font-semibold">เริ่มต้นเปลี่ยนสังคม</span>
-					<div class="w-24 h-20 bg-namjaiyellow rounded-b-full lg:hidden"></div>
+					<div class="w-24 h-20 md:w-28 md:h-24 bg-namjaiyellow rounded-b-full lg:hidden"></div>
 				</div>
-				<div class="w-16 border-4 border-t border-namjaired lg:mt-2"></div>
+				<div class="w-16 border-4 border-t border-namjaired md:-mt-4 lg:mt-2"></div>
 				<!-- <div class="lg:grid lg:grid-cols-4 lg:gap-8 lg:mt-10"> -->
 				<div class="hidden lg:inline-flex lg:space-x-[60px] lg:mt-10 ">
 					<div class="w-32 h-10 border border-namjaibrown rounded-md py-2 text-center space-x-3">
@@ -44,7 +44,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 						<span class="font-medium">มูลนิธิ</span>
 					</div>
 				</div>
-				<w-form name="donorForm" class="py-8 lg:py-10 ">
+				<w-form name="donorForm" class="py-4 lg:py-10 ">
 					<div class="space-y-8 lg:space-y-10">
 						<div class="space-y-8 lg:grid lg:grid-cols-2 lg:gap-8 lg:space-y-0">
 							<div class="space-y-2 lg:space-y-4">
@@ -76,7 +76,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 				</w-form>
 
 				<w-form name="donorForm" class="hidden lg:inline">
-					<div class="space-y-10 my-10">
+					<div class="space-y-10">
 						<div class="space-y-4">
 							<div class="font-medium">ขนาดองค์กร</div>
 							<div class="inline-flex space-x-10">
