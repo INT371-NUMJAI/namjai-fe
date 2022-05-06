309ea39681f14bb503aecee0eeb8f97f9b58b8bb
fah <supanan.c@mail.kmutt.ac.th>
2022-05-06 22:06:45 +0700

    create component drawer

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,18 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
-</script>
-
 <template>
 	<w-app>
-		<header>
-			<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-			<w-icon xl color="primary"> fa fa-star </w-icon>
-			<w-button class="ml-24" bg-color="success">success</w-button>
-			<w-button class="ml-24" bg-color="error">error</w-button>
-
-			<div class="bg-slate-500">
-				<h1 class="text-3xl underline text-namjaigreen font-ibm-plex-thai">สวัสดี ฟอนต์ ไอบีเอ็มใหม่!</h1>
-				<br />
-
-				<h1 class="text-3xl underline text-green-500 font-ibm-plex-thai">HelloWorld</h1>
-				<h1 class="text-3xl underline text-green-500">HelloWorld</h1>
-
-				<HelloWorld msg="You did it!" />
-
-				<nav>
-					<RouterLink to="/">Home</RouterLink>
-					<RouterLink to="/about">About</RouterLink>
-				</nav>
-			</div>
-			<!-- <h1 class="text-3xl underline text-green-500 font-ibmPlex">Hello world!</h1>
-
-		<h1 class="text-3xl underline text-green-500">Hello world!</h1> -->
-		</header>
-
-		<RouterView />
+		<div class="text-namjaiblack">
+			<p class="text-3xl text-center">Hello test</p>
+		</div>
+		<div>
+		<w-button>
+			<w-icon color="black">fa fa-bars</w-icon>
+		</w-button>
+		</div>
+		<w-drawer>
+			<w-button>มูลนิธิ</w-button>
+			<w-button>โครงการ</w-button>
+			<w-button>จิตอาสา</w-button>
+			<w-button>เกี่ยวกับเรา</w-button>
+		</w-drawer>
 	</w-app>
 </template>
