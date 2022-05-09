d374e0632dcaf7ae9c241656de907d0465eca9ef
fah <supanan.c@mail.kmutt.ac.th>
2022-05-09 15:15:48 +0700

    edit component NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,13 @@
 <template>
 	<w-app>
-		<nav class="bg-namjaigreen flex items-center justify-between px-7 py-3 lg:py-3 lg:px-11 drop-shadow-md lg:fixed lg:w-screen">
+		<nav class="bg-namjaigreen flex items-center justify-between px-7 py-6 lg:py-4 lg:px-11 drop-shadow-md lg:fixed lg:w-screen">
             <div class="flex items-center text-white">
                 <img class="w-28" src="../assets/namjai.png" />
             </div>
             <div class="lg:hidden">
                 <w-icon color="white">fa fa-bars</w-icon>
             </div>
-            <div class="hidden lg:flex lg:space-x-11 items-center text-white font-semibold">
+            <div class="hidden lg:flex lg:space-x-11 items-center text-white lg:text-sm">
                 <w-button bg-color="transparent" class="font-medium">มูลนิธิ</w-button>
                 <w-button bg-color="transparent" class="font-medium">โครงการ</w-button>
                 <w-button bg-color="transparent" class="font-medium">จิตอาสา</w-button>
