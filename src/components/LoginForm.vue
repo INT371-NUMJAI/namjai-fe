f9f3abfaad15b5ddbf7f2a9933e400ee497e345a
fah <supanan.c@mail.kmutt.ac.th>
2022-05-09 16:58:21 +0700

    edit component LoginForm

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,54 +1,42 @@
 <template>
 	<w-app>
-		<nav class="bg-namjaigreen flex items-center justify-between px-7 py-3 lg:py-3 lg:px-11 drop-shadow-md lg:fixed lg:w-screen">
-			<div class="flex items-center text-white">
-				<img class="w-28" src="../assets/namjai.png" />
+		<div class="lg:flex md:justify-end hidden">
+			<div class="lg:w-1/3 lg:h-screen lg:bg-namjaigreen">
 			</div>
-			<div class="lg:hidden">
-				<w-icon color="white">fa fa-bars</w-icon>
+		</div>
+		<div
+			class="bg-namjaiwhite lg:bg-transparent lg:mx-28 md:px-20 pt-10 pb-10 px-[30px] absolute bottom-0 lg:pt-[120px] lg:h-screen h-3/5 md:h-1/2 w-full lg:w-fit rounded-t-3xl md:rounded-t-3xl">
+			<div class="-my-[40px] lg:hidden float-right">
+				<div class="w-24 h-20 bg-namjaiyellow rounded-b-full"></div>
 			</div>
-			<div class="hidden lg:flex lg:space-x-11 items-center text-white font-semibold">
-				<w-button bg-color="transparent" class="font-medium">มูลนิธิ</w-button>
-				<w-button bg-color="transparent" class="font-medium">โครงการ</w-button>
-				<w-button bg-color="transparent" class="font-medium">จิตอาสา</w-button>
-				<w-button bg-color="transparent" class="font-medium">เกี่ยวกับเรา</w-button>
-				<w-button bg-color="transparent" round outline class="py-4 font-medium">เข้าสู่ระบบ</w-button>
+			<h1 class="text-namjaiblack font-semibold text-2xl lg:text-5xl">ยินดีต้อนรับ!</h1>
+			<div class="mt-2">
+				<div class="w-16 border-4 border-t border-namjaired"></div>
 			</div>
-		</nav>
-			<div class="lg:flex md:justify-end hidden">
-				<div class="lg:w-1/3 lg:h-screen lg:bg-namjaigreen">
-				</div>
+			<div class="mt-7 font-medium lg:w-80 lg:mt-10">
+				<w-form class="grid grid-flow-row">
+					<label class="text-sm lg:text-base mb-2">อีเมล</label>
+					<w-input class="mb-8" type="email" color="black" placeholder="เช่น abc@gmail.com" />
+					<label class="text-sm lg:text-base mb-2">รหัสผ่าน</label>
+					<w-input class="mb-6" type="password" color="black" placeholder="*******" />
+				</w-form>
 			</div>
-			<div
-				class="bg-namjaiwhite lg:bg-transparent lg:mx-28 md:px-20 p-[30px] absolute bottom-0 lg:pt-[120px] lg:h-screen h-3/5 md:h-1/2 w-full lg:w-fit rounded-t-3xl md:rounded-t-3xl">
-				<div class="-my-[30px] lg:hidden float-right">
-					<div class="w-24 h-20 bg-namjaiyellow rounded-b-full"></div>
+			<div class="flex justify-end mb-5">
+					<button class="-mt-2 text-sm lg:text-base">ลืมรหัสผ่าน?</button>
 				</div>
-				<h1 class="text-namjaiblack font-bold text-2xl lg:text-5xl">ยินดีต้อนรับ!</h1>
-				<div class="py-4">
-					<div class="w-16 border-4 border-t border-namjaired"></div>
-				</div>
-				<div class="mt-7 font-medium lg:w-80 lg:mt-10">
-					<w-form class="grid grid-flow-row">
-						<label class="text-sm mb-2">อีเมล</label>
-						<w-input class="mb-10" type="email" color="black" placeholder="เช่น abc@gmail.com" />
-						<label class="text-sm mb-2">รหัสผ่าน</label>
-						<w-input class="mb-10" type="password" color="black" placeholder="*******" />
-					</w-form>
-				</div>
-				<div class="lg:block lg:w-80">
-				<div class="md:mx-auto mx-auto flex justify-center mt-3 mb-5 bg-namjaigreen h-14 my-10 rounded-xl">
-					<w-button color="white" bg-color="transparent" class="text-white text-base font-semibold"
+			<div class="lg:block lg:w-80">
+				<div class="md:mx-auto mx-auto flex justify-center mt-3 mb-5 bg-namjaigreen h-[60px] my-10 rounded-xl">
+					<w-button color="white" bg-color="transparent" class="text-white text-lg font-semibold"
 						type="submit">
 						เข้าสู่ระบบ</w-button>
 				</div>
-				<div class="flex justify-center text-sm">
+				<div class="flex justify-center text-sm lg:text-base">
 					<p>ยังไม่มีบัญชีใช่หรือไม่?</p>
 					<button bg-color="transparent" class="ml-1 font-semibold">ลงทะเบียน</button>
 				</div>
-				</div>
 			</div>
-			<img class="hidden lg:block lg:w-2/5 lg:absolute lg:top-44 lg:right-48 lg:z-10" src="../src/assets/pic1.png" />
-			<img class="hidden lg:block lg:w-3/5 lg:absolute lg:top-24 lg:right-14 lg:-z-0" src="../src/assets/pic2.png" />			
+		</div>
+		<img class="hidden lg:block lg:w-2/5 lg:absolute lg:top-44 lg:right-48 lg:z-10" src="../src/assets/pic1.png" />
+		<img class="hidden lg:block lg:w-3/5 lg:absolute lg:top-24 lg:right-14 lg:-z-0" src="../src/assets/pic2.png" />
 	</w-app>
 </template>
