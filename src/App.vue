e00cbe3ca6a5570c408d48a1c63435ca55d98ccd
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2022-05-09 16:37:23 +0700

    [EDIT] space on siugnup form

--- src/App.vue
+++ src/App.vue
@@ -28,8 +28,8 @@ import HelloWorld from "@/components/HelloWorld.vue";
 		</div>
 		<div class="h-full w-full md:h-[850px] bg-namjaiwhite rounded-2xl lg:bg-transparent">
 			<div class="mx-8 mb-20 md:mx-20 lg:mx-44">
-				<div class="inline-flex space-x-9 md:space-x-[380px]">
-					<span class="mt-10 text-2xl lg:text-[48px] lg:mt-16 font-semibold">เริ่มต้นเปลี่ยนสังคม</span>
+				<div class="flex space-x-9 md:space-x-[380px]">
+					<span class="mt-10 text-2xl lg:text-5xl lg:mt-16 font-semibold">เริ่มต้นเปลี่ยนสังคม</span>
 					<div class="w-24 h-20 md:w-28 md:h-24 bg-namjaiyellow rounded-b-full lg:hidden"></div>
 				</div>
 				<div class="w-16 border-4 border-t border-namjaired md:-mt-4 lg:mt-2"></div>
@@ -44,31 +44,31 @@ import HelloWorld from "@/components/HelloWorld.vue";
 						<span class="font-medium">มูลนิธิ</span>
 					</div>
 				</div>
-				<w-form name="donorForm" class="py-4 lg:py-10 ">
+				<w-form name="donorForm" class="py-7 lg:py-10 ">
 					<div class="space-y-8 lg:space-y-10">
 						<div class="space-y-8 lg:grid lg:grid-cols-2 lg:gap-8 lg:space-y-0">
 							<div class="space-y-2 lg:space-y-4">
-								<label class="font-medium">ชื่อ</label>
+								<label class="text-sm lg:text-base font-medium">ชื่อ</label>
 								<w-input color="black" placeholder="ชื่อ"></w-input>
 							</div>
 							<div class="space-y-2 lg:space-y-4">
-								<label class="font-medium">นามสกุล</label>
+								<label class="text-sm lg:text-base font-medium">นามสกุล</label>
 								<w-input color="black" placeholder="นามสกุล"></w-input>
 							</div>
 						</div>
 
 						<div class="space-y-2 lg:space-y-4 ">
-							<label class="font-medium">อีเมล</label>
+							<label class="text-sm lg:text-base font-medium">อีเมล</label>
 							<w-input color="black" placeholder="อีเมล"></w-input>
 						</div>
 
 						<div class="space-y-8 lg:grid lg:grid-cols-2 lg:gap-8 lg:space-y-0">
 							<div class="space-y-2 lg:space-y-4">
-								<label class="font-medium">ชื่อผู้ใช้</label>
+								<label class="text-sm lg:text-base font-medium">ชื่อผู้ใช้</label>
 								<w-input color="black" placeholder="ชื่อผู้ใช้"></w-input>
 							</div>
 							<div class="space-y-2 lg:space-y-4">
-								<label class="font-medium">รหัสผ่าน</label>
+								<label class="text-sm lg:text-base font-medium">รหัสผ่าน</label>
 								<w-input color="black" placeholder="รหัสผ่าน"></w-input>
 							</div>
 						</div>
@@ -166,7 +166,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 						</div>
 					</div>
 				</w-form>
-				<div class="flex justify-center text-sm space-x-2 mx-10">
+				<div class="flex justify-center text-sm lg:text-base space-x-2 mx-10">
 					<p>มีบัญชีแล้วใช่หรือไม่?</p>
 					<button bg-color="transparent" class="ml-1 font-semibold">เข้าสู่ระบบ</button>
 				</div>
