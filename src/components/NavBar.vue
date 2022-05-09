f878f0016c6cce2da6c5a6f4d9311117645051e4
fah <supanan.c@mail.kmutt.ac.th>
2022-05-09 16:14:12 +0700

    uninstall wave ui

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,5 @@
 <template>
-	<w-app>
-		<nav class="bg-namjaigreen flex items-center justify-between px-7 py-6 lg:py-4 lg:px-11 drop-shadow-md lg:fixed lg:w-screen">
+		<nav class="bg-namjaigreen flex items-center justify-between px-7 py-6 lg:py-2 lg:px-11 drop-shadow-md lg:fixed lg:w-screen">
             <div class="flex items-center text-white">
                 <img class="w-28" src="../assets/namjai.png" />
             </div>
@@ -8,12 +7,11 @@
                 <w-icon color="white">fa fa-bars</w-icon>
             </div>
             <div class="hidden lg:flex lg:space-x-11 items-center text-white lg:text-sm">
-                <w-button bg-color="transparent" class="font-medium">มูลนิธิ</w-button>
-                <w-button bg-color="transparent" class="font-medium">โครงการ</w-button>
-                <w-button bg-color="transparent" class="font-medium">จิตอาสา</w-button>
-                <w-button bg-color="transparent" class="font-medium">เกี่ยวกับเรา</w-button>
-                <w-button bg-color="transparent" round outline class="py-4 font-medium">เข้าสู่ระบบ</w-button>
+                <button bg-color="transparent" class="font-medium">มูลนิธิ</button>
+                <button bg-color="transparent" class="font-medium">โครงการ</button>
+                <button bg-color="transparent" class="font-medium">จิตอาสา</button>
+                <button bg-color="transparent" class="font-medium">เกี่ยวกับเรา</button>
+                <button bg-color="transparent" round outline class="py-4 font-medium">เข้าสู่ระบบ</button>
             </div>
         </nav>
-	</w-app>
 </template>
