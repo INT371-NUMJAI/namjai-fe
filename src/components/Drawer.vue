f96ed50bdfa8f98ec3f101399d7d08374dc863e0
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2022-05-10 21:09:41 +0700

    edit drawer

---
+++ src/components/Drawer.vue
@@ -0,0 +1,64 @@
+<template>
+	<div>
+		<w-button @click="showDrawer = true" bg-color="transparent">
+			<w-icon color="black">fa fa-bars</w-icon>
+		</w-button>
+		<w-drawer v-model="showDrawer" bg-color="namjaigreen" width="187">
+			<w-button
+				class="button--close"
+				@click="showDrawer = false"
+				sm
+				outline
+				round
+				absolute
+				color="black"
+				icon="wi-cross"
+			></w-button>
+			<div class="space-y-6 mx-5 mt-10 w-96">
+				<div class="flex justify-center">
+					<w-button bg-color="transparent" outline round class="text-black py-1">เข้าสู่ระบบ</w-button>
+				</div>
+				<div class="flex justify-center">
+					<w-button bg-color="transparent" outline round class="text-black py-1">สมัครสมาชิก</w-button>
+				</div>
+				<w-divider class="w-36" color="grey" />
+				<div class="space-y-7 -ml-2">
+					<div class="flex justify-end">
+						<w-button bg-color="transparent" color="info-light1" class="block w-auto">มูลนิธิ </w-button>
+					</div>
+					<div class="flex justify-end">
+						<w-button bg-color="transparent" color="info-light1" class="block w-auto">โครงการ </w-button>
+					</div>
+					<div class="flex justify-end">
+						<w-button bg-color="transparent" color="info-light1" class="block w-auto">จิตอาสา </w-button>
+					</div>
+					<div class="flex justify-end">
+						<w-button bg-color="transparent" color="info-light1" class="block w-auto">เกี่ยวกับเรา </w-button>
+					</div>
+				</div>
+				<div class="flex justify-center">
+					<w-button outline round bg-color="transparent" color="black" class="block w-auto mt-10">ออกจากระบบ </w-button>
+				</div>
+			</div>
+		</w-drawer>
+	</div>
+</template>
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+	data() {
+		return {
+			showDrawer: false,
+		};
+	},
+});
+</script>
+<style scoped>
+.namjaigreen--bg {
+	background-color: #00715d;
+}
+
+.navy-blue--bg {
+	background-color: #345276;
+}
+</style>
