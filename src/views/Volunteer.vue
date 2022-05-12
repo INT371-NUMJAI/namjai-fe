2e676025e14c6363de9ed1ee22919a6ec65417b9
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2022-05-12 21:28:53 +0700

    [ADD] Volunteer page and edit router

---
+++ src/views/Volunteer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <volunteer-activity-block />
+        <div class="flex justify-center py-[60px]">
+             <button class="bg-transparent hover:bg-namjaigreen text-namjaigreen font-medium hover:text-white py-2 px-10 border-2 border-namjaigreen hover:border-transparent rounded">เพิ่มเติม</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import VolunteerActivityBlock from '../components/VolunteerActivityBlock.vue'
+
+export default {
+    components:{
+        "volunteer-activity-block": VolunteerActivityBlock,
+    },
+}
+</script>
