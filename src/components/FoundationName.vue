4252cd94e78b92f5947df800ac8728366f4bb825
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2022-05-13 13:54:29 +0700

    [ADD] FoundationName component

---
+++ src/components/FoundationName.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mt-[120px] mx-[30px] flex flex-wrap items-center justify-center">
+    <div
+      class="container sm:w-full md:w-2/3 bg-namjaiwhite rounded-xl transform duration-200 easy-in-out"
+    >
+      <div class="flex justify-center px-5 -mt-12">
+        <img
+          class="h-32 w-32 bg-namjaibeige p-2 rounded-full object-cover"
+          src="../assets/pic1.png"
+          alt=""
+        />
+      </div>
+
+      <div class="text-center pt-3 pb-6">
+        <h2 class="text-namjaiblack text-2xl font-semibold">มูลนิธิกระจกเงา</h2>
+      </div>
+    </div>
+  </div>
+</template>
