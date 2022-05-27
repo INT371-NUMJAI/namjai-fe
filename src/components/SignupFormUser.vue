9054f1bc4e5eb335d75130dd364762c98189e228
fah <supanan.c@mail.kmutt.ac.th>
2022-05-27 18:24:37 +0700

    edit validation

--- src/components/SignupFormUser.vue
+++ src/components/SignupFormUser.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-10">
-    <w-form name="donorForm" class="py-7 lg:py-10">
+    <w-form v-model="valid" name="donorForm" class="py-7 lg:py-10">
       <div class="space-y-8 lg:space-y-10">
         <div class="space-y-8 lg:grid lg:grid-cols-2 lg:gap-8 lg:space-y-0">
           <div class="space-y-2 lg:space-y-4">
@@ -22,12 +22,21 @@
         </div>
         <div class="space-y-2 lg:space-y-4">
           <label class="text-sm lg:text-base font-medium">อีเมล</label>
-          <w-input :validators="[validators.required]" type="email" color="black" placeholder="อีเมล"></w-input>
+          <w-input
+            :validators="[validators.required]"
+            type="email"
+            color="black"
+            placeholder="อีเมล"
+          ></w-input>
         </div>
         <div class="space-y-8 lg:grid lg:grid-cols-2 lg:gap-8 lg:space-y-0">
           <div class="space-y-2 lg:space-y-4">
             <label class="text-sm lg:text-base font-medium">ชื่อผู้ใช้</label>
-            <w-input :validators="[validators.required, validators.username]" color="black" placeholder="ชื่อผู้ใช้"></w-input>
+            <w-input
+              :validators="[validators.required, validators.username]"
+              color="black"
+              placeholder="ชื่อผู้ใช้"
+            ></w-input>
           </div>
           <div class="space-y-2 lg:space-y-4">
             <label class="text-sm lg:text-base font-medium">รหัสผ่าน</label>
@@ -39,31 +48,31 @@
             ></w-input>
           </div>
         </div>
-      </div>
-    <div class="lg:block lg:w-80 lg:mt-10 lg:pb-[60px] lg:mx-auto">
-      <div
-        class="md:mx-auto mx-auto flex justify-center mt-3 mb-5 bg-namjaigreen h-[60px] my-10 rounded-xl"
-      >
-        <w-button
-          :disabled="[valid === false]"
-          color="white"
-          bg-color="transparent"
-          class="text-white text-lg font-semibold"
-          type="submit"
+        <div
+          class="md:mx-auto mx-auto flex justify-center mt-3 mb-5 bg-namjaigreen h-[60px] lg:w-80 my-10 rounded-xl"
         >
-          ลงทะเบียน
-        </w-button>
+          <w-button
+            :disabled="valid === false"
+            color="white"
+            bg-color="transparent"
+            class="text-white text-lg font-semibold"
+            type="submit"
+          >
+            ลงทะเบียน
+          </w-button>
+        </div>
       </div>
-      <div class="flex justify-center text-sm lg:text-base">
-        <p>ยังไม่มีบัญชีใช่หรือไม่?</p>
-        <router-link to="/login">
-          <button bg-color="transparent" class="ml-1 font-semibold">
-            เข้าสู่ระบบ
-          </button>
-        </router-link>
+      </w-form>
+      <div class="lg:block lg:w-80 lg:mt-10 lg:pb-[60px] lg:mx-auto">
+        <div class="flex justify-center text-sm lg:text-base">
+          <p>ยังไม่มีบัญชีใช่หรือไม่?</p>
+          <router-link to="/login">
+            <button bg-color="transparent" class="ml-1 font-semibold">
+              เข้าสู่ระบบ
+            </button>
+          </router-link>
+        </div>
       </div>
-    </div>
-        </w-form>
   </div>
 </template>
 
@@ -75,9 +84,10 @@ export default defineComponent({
     // userlists: ["Qwanjai", "Faahhhhhhh", "Chutipaaaa"],
     validators: {
       required: (value) => !!value || "This field is required",
-      minLength: (value) => value.length >= 8 || "Your password must be minimum 8 characters",
+      minLength: (value) =>
+        value.length >= 8 || "Your password must be minimum 8 characters",
       username: (value) => {
-        let userlists = ["Qwanjai", "Faahhhhhhh", "Chutipaaaa"]
+        let userlists = ["Qwanjai", "Faahhhhhhh", "Chutipaaaa"];
         for (let i = 0; i < userlists.length; i++) {
           if (userlists[i].toLowerCase() === value.toLowerCase()) {
             return "This username is already in use";
