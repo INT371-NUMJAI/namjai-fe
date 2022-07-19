60a0720c3954292884caa5f525c98965db6aa000
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2022-07-19 21:03:04 +0700

    feat: integrate api login

--- src/components/Account/LoginForm.vue
+++ src/components/Account/LoginForm.vue
@@ -3,9 +3,7 @@ routerlink
 	<div class="lg:flex md:justify-end hidden">
 		<div class="lg:w-1/3 lg:h-screen lg:bg-namjaigreen"></div>
 	</div>
-	<div
-		class="bg-namjaiwhite lg:bg-transparent lg:absolute lg:top-[60px] lg:mx-28 md:px-20 pt-10 pb-10 lg:pb-0 px-[30px] mt-[80%] lg:mt-0 lg:pt-[120px] lg:h-screen h-3/5 md:h-1/2 w-full lg:w-fit rounded-t-3xl md:rounded-t-3xl"
-	>
+	<div class="bg-namjaiwhite lg:bg-transparent lg:absolute lg:top-[60px] lg:mx-28 md:px-20 pt-10 pb-10 lg:pb-0 px-[30px] mt-[80%] lg:mt-0 lg:pt-[120px] lg:h-screen h-3/5 md:h-1/2 w-full lg:w-fit rounded-t-3xl md:rounded-t-3xl">
 		<div class="-my-[40px] lg:hidden float-right">
 			<div class="w-24 h-20 bg-namjaiyellow rounded-b-full"></div>
 		</div>
@@ -16,34 +14,23 @@ routerlink
 		<div class="mt-7 font-medium lg:w-80 lg:mt-10">
 			<w-form class="grid grid-flow-row">
 				<label class="text-sm lg:text-base mb-2">อีเมล</label>
-				<w-input
-					:validators="[validators.required]"
-					class="mb-8"
-					type="email"
-					color="black"
-					placeholder="เช่น abc@gmail.com"
-				/>
+				<w-input :validators="[validators.required]" class="mb-8" type="email" color="black" placeholder="เช่น abc@gmail.com" v-model="loginRequest.email" />
 				<label class="text-sm lg:text-base mb-2">รหัสผ่าน</label>
-				<w-input :validators="[validators.required]" class="mb-6" type="password" color="black" placeholder="*******" />
+				<w-input :validators="[validators.required]" class="mb-6" type="password" color="black" placeholder="*******" v-model="loginRequest.password" />
 				<div class="flex justify-end mb-5">
 					<button class="-mt-2 text-sm lg:text-base underline underline-offset-2">ลืมรหัสผ่าน?</button>
+					{{ loginRequest }}
 				</div>
 				<!-- <base-button class="w-80" /> -->
 				<div class="md:mx-auto mx-auto flex justify-center mt-3 mb-5 bg-namjaigreen h-[60px] lg:w-80 my-10 rounded-xl">
-					<w-button color="white" bg-color="transparent" class="text-white text-lg font-semibold" type="submit">
-						เข้าสู่ระบบ</w-button
-					>
+					<w-button color="white" bg-color="transparent" class="text-white text-lg font-semibold" type="submit" @click="submitLogin"> เข้าสู่ระบบ</w-button>
 				</div>
 			</w-form>
 		</div>
 		<div class="lg:block lg:w-80">
 			<div class="flex justify-center text-sm lg:text-base">
 				<p>ยังไม่มีบัญชีใช่หรือไม่?</p>
-				<router-link to="/signup"
-					><button bg-color="transparent" class="ml-1 font-semibold underline underline-offset-2">
-						ลงทะเบียน
-					</button></router-link
-				>
+				<router-link to="/signup"><button bg-color="transparent" class="ml-1 font-semibold underline underline-offset-2">ลงทะเบียน</button></router-link>
 			</div>
 		</div>
 	</div>
@@ -53,12 +40,33 @@ routerlink
 
 <script>
 import { defineComponent } from "vue";
-
+import authService from "@/services/auth-service";
 export default defineComponent({
 	data: () => ({
 		validators: {
 			required: (value) => !!value || "This field is required",
 		},
+		loginRequest: {
+			email: "",
+			password: "",
+		},
 	}),
+	methods: {
+		submitLogin() {
+			console.log(this.loginRequest);
+			authService
+				.login(this.loginRequest)
+				.then((response) => {
+					console.log(response);
+					// if (response.status == 200) {
+					// 	console.log(response.data);
+					// 	// this.$waveui.notify("sign up profile successfully", "success");
+					// }
+				})
+				.catch((error) => {
+					console.error(error);
+				});
+		},
+	},
 });
 </script>
