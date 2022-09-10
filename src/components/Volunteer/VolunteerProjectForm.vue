f89c5ec318816d8574fdad68b28a99614ee4f870
fah <supanan.c@mail.kmutt.ac.th>
2022-09-11 01:35:18 +0700

    [EDIT] v-model object VolunteerProjectForm

--- src/components/Volunteer/VolunteerProjectForm.vue
+++ src/components/Volunteer/VolunteerProjectForm.vue
@@ -2,6 +2,7 @@
   <div
     class="mx-[30px] md:mx-[40px] lg:mx-[177px] mt-[60px] lg:mt-[120px] h-auto"
   >
+  {{ volunteerAttendanceBody }}
     <h1 class="text-2xl lg:text-2xl mb-[40px]">อาสาจัดเต็มช่วยขนย้ายสิ่งของและต้อนรับผู้บริจาค</h1>
     <div class="lg:mx-[183px] lg:mt-[60px]">
     <w-form v-model="valid">
@@ -14,6 +15,7 @@
         label="ชื่อ"
         label-color="black"
         placeholder=" "
+        v-model="volunteerAttendanceBody.fname"
       />
       <w-input
         :validators="[validators.required]"
@@ -23,6 +25,7 @@
         label="นามสกุล"
         label-color="black"
         placeholder=" "
+        v-model="volunteerAttendanceBody.lname"
       />
       </div>
       <div class="flex space-x-[20px] lg:space-x-[30px]">
@@ -34,6 +37,7 @@
         label="ชื่อเล่น"
         label-color="black"
         placeholder=" "
+        v-model="volunteerAttendanceBody.nickname"
       />
         <w-input
           :validators="[validators.required]"
@@ -43,6 +47,7 @@
           label="อายุ"
           label-color="black"
           placeholder=" "
+          v-model="volunteerAttendanceBody.age"
         />
         </div>
         <div class="flex space-x-[20px] lg:space-x-[30px]">
@@ -54,6 +59,7 @@
           label="เบอร์โทรศัพท์"
           label-color="black"
           placeholder=" "
+          v-model="volunteerAttendanceBody.phoneNumber"
         />
         <!-- <label class="text-sm">แขวง</label> -->
         <w-input
@@ -64,6 +70,7 @@
           label="อีเมล"
           label-color="black"
           placeholder=" "
+          v-model="volunteerAttendanceBody.email"
         />
         </div>
       <base-button
@@ -84,7 +91,15 @@ export default {
   setup() {
     const valid = ref(null);
     const { validators } = useValidation();
-    return { validators, valid };
+    const volunteerAttendanceBody = reactive({
+      fname: "",
+      lname: "",
+      nickname: "",
+      age: "",
+      phoneNumber: "",
+      email: "",
+    });
+    return { validators, valid, volunteerAttendanceBody };
   },
 };
 </script>
