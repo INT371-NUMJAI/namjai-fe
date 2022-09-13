edda4b66681471ffc779c847a0b777bc5467ec6c
fah <supanan.c@mail.kmutt.ac.th>
2022-09-14 00:11:24 +0700

    [FEAT] disable button

--- src/components/Project/ProjectAdd.vue
+++ src/components/Project/ProjectAdd.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="space-y-2.5 lg:space-y-5">
-    <button @click="routeToProjectForm" class="bg-namjaigreen w-full lg:w-[186px] py-3 flex justify-center space-x-3 rounded-lg">
+    <button :disabled="use_auth.store_auth.user.status === `DISABLE` && use_auth.store_auth.status.loggedIn" @click="routeToProjectForm" class="bg-namjaigreen w-full lg:w-[186px] py-3 flex justify-center space-x-3 rounded-lg">
       <w-icon class="mr1" md color="white">fa fa-plus</w-icon>
       <p class="text-[14px] text-white">เพิ่มโครงการบริจาค</p>
     </button>
@@ -11,6 +11,7 @@
 <script>
 import ProjectCard from "./ProjectCard.vue";
 import { useRouter } from "vue-router";
+import { useAuth } from "../../services/auth-middleware";
 
 export default {
   components: {
@@ -18,10 +19,12 @@ export default {
   },
   setup() {
     const router = useRouter();
+    const use_auth = useAuth();
+
     const routeToProjectForm = () => {
       router.push(`/add/foundationproject`);
     }
-    return { routeToProjectForm }
+    return { routeToProjectForm, use_auth }
   }
 };
 </script>
