4938bcf6047f4bae8a2e34f799197583ba750450
Qwanjai Deelertpradit <d_khonjai@hotmail.com>
2022-09-14 15:22:26 +0700

    uninstall cypress

--- src/components/Verification/AdminVerifyAccountDetail.vue
+++ src/components/Verification/AdminVerifyAccountDetail.vue
@@ -98,8 +98,6 @@ export default {
 		const route = useRoute();
 		// const fdnVerify = reactive({ addressDetail: "", approval: "", contactNumber: "", createDate: "", district: "", email: "", establishDate: "", fdnDetail: "", fdnName: "", fdnSize: "", fdnUUid: "", founderName: "", postalCode: "", province: "", resource: null, status: "", subDistrict: "" });
 		const fdnVerify = ref({});
-		const fdnStatus = ref("");
-
 		const fetchFoundationDetail = (id) => {
 			approveService.getFDNByID(id).then((response) => {
 				fdnVerify.value = response.data;
