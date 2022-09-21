c4fbddf572181c3e237a017e3e96227a1c305950
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2022-09-21 22:09:09 +0700

    [ADD] foundation suggestion component

---
+++ src/components/FoundationSuggestion/Suggestion.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="my-[60px] lg:mt-[120px] mx-[30px] lg:mx-[177px]">
+    <div class="space-y-[10px] lg:space-y-5">
+      <h1 class="text-[25px] lg:text-[30px]">หมวดหมู่การทำบุญ</h1>
+      <p class="leading-relaxed lg:text-[20px]">เลือกหมวดหมู่ที่คุณสนใจ เพื่อให้เรานำเสนอได้ตรงใจคุณมากขึ้น</p>
+    </div>
+    <div class="grid grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] pt-[30px] lg:pt-[40px]">
+      <div class="bg-white space-y-4 lg:space-y-6 rounded-2xl px-4 py-4 drop-shadow-md hover:shadow-md active:bg-namjaiyellow">
+        <div class="w-[85px] lg:w-1/2 h-[85px] lg:h-[120px] flex mx-auto ">
+          <img src="" class="object-cover object-top  w-full" />
+        </div>
+        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การแพทย์</h2>
+      </div>
+    </div>
+    <div class="pt-[40px] lg:pt-[60px] flex justify-center gap-5 lg:gap-10">
+      <button class="px-4 py-2 w-1/2 lg:w-1/6 text-gray-50 text-lg rounded-lg bg-gray-300">ข้าม</button>
+      <button class="px-4 py-2 w-1/2 lg:w-1/6 text-gray-50 text-lg rounded-lg bg-namjaigreen">เสร็จสิ้น</button>
+    </div>
+  </div>
+</template>
