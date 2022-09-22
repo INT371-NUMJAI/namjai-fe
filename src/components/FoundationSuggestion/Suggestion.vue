5d055f1c1c4a5a0bbd0de7bbd7adbec0b8ecffce
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2022-09-22 23:57:17 +0700

    [EDIT] catagories name

--- src/components/FoundationSuggestion/Suggestion.vue
+++ src/components/FoundationSuggestion/Suggestion.vue
@@ -11,69 +11,69 @@
         </div>
         <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การแพทย์</h2>
       </div>
-      <div class="bg-white space-y-4 lg:space-y-6 rounded-2xl px-4 py-4 drop-shadow-md hover:shadow-md active:bg-namjaiyellow">
+      <div class="bg-white bg- space-y-4 lg:space-y-6 rounded-2xl px-4 py-4 drop-shadow-md hover:shadow-md active:bg-namjaiyellow">
         <div class="w-[85px] lg:w-1/2 h-[85px] lg:h-[120px] flex mx-auto ">
           <img src="" class="object-cover object-top  w-full" />
         </div>
-        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การแพทย์</h2>
+        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การศึกษา</h2>
       </div>
       <div class="bg-white space-y-4 lg:space-y-6 rounded-2xl px-4 py-4 drop-shadow-md hover:shadow-md active:bg-namjaiyellow">
         <div class="w-[85px] lg:w-1/2 h-[85px] lg:h-[120px] flex mx-auto ">
           <img src="" class="object-cover object-top  w-full" />
         </div>
-        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การแพทย์</h2>
+        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">เด็กและสตรี</h2>
       </div>
       <div class="bg-white space-y-4 lg:space-y-6 rounded-2xl px-4 py-4 drop-shadow-md hover:shadow-md active:bg-namjaiyellow">
         <div class="w-[85px] lg:w-1/2 h-[85px] lg:h-[120px] flex mx-auto ">
           <img src="" class="object-cover object-top  w-full" />
         </div>
-        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การแพทย์</h2>
+        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">ผู้พิการ</h2>
       </div>
       <div class="bg-white space-y-4 lg:space-y-6 rounded-2xl px-4 py-4 drop-shadow-md hover:shadow-md active:bg-namjaiyellow">
         <div class="w-[85px] lg:w-1/2 h-[85px] lg:h-[120px] flex mx-auto ">
           <img src="" class="object-cover object-top  w-full" />
         </div>
-        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การแพทย์</h2>
+        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">ผู้สูงอายุ</h2>
       </div>
       <div class="bg-white space-y-4 lg:space-y-6 rounded-2xl px-4 py-4 drop-shadow-md hover:shadow-md active:bg-namjaiyellow">
         <div class="w-[85px] lg:w-1/2 h-[85px] lg:h-[120px] flex mx-auto ">
           <img src="" class="object-cover object-top  w-full" />
         </div>
-        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การแพทย์</h2>
+        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">พัฒนาชุมชน</h2>
       </div>
       <div class="bg-white space-y-4 lg:space-y-6 rounded-2xl px-4 py-4 drop-shadow-md hover:shadow-md active:bg-namjaiyellow">
         <div class="w-[85px] lg:w-1/2 h-[85px] lg:h-[120px] flex mx-auto ">
           <img src="" class="object-cover object-top  w-full" />
         </div>
-        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การแพทย์</h2>
+        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">ศิลปะและวัฒนธรรม</h2>
       </div>
       <div class="bg-white space-y-4 lg:space-y-6 rounded-2xl px-4 py-4 drop-shadow-md hover:shadow-md active:bg-namjaiyellow">
         <div class="w-[85px] lg:w-1/2 h-[85px] lg:h-[120px] flex mx-auto ">
           <img src="" class="object-cover object-top  w-full" />
         </div>
-        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การแพทย์</h2>
+        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">สัตว์</h2>
       </div>
       <div class="bg-white space-y-4 lg:space-y-6 rounded-2xl px-4 py-4 drop-shadow-md hover:shadow-md active:bg-namjaiyellow">
         <div class="w-[85px] lg:w-1/2 h-[85px] lg:h-[120px] flex mx-auto ">
           <img src="" class="object-cover object-top  w-full" />
         </div>
-        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การแพทย์</h2>
+        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">สิ่งแวดล้อม</h2>
       </div>
       <div class="bg-white space-y-4 lg:space-y-6 rounded-2xl px-4 py-4 drop-shadow-md hover:shadow-md active:bg-namjaiyellow">
         <div class="w-[85px] lg:w-1/2 h-[85px] lg:h-[120px] flex mx-auto ">
           <img src="" class="object-cover object-top  w-full" />
         </div>
-        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การแพทย์</h2>
+        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">สิทธิมนุษยชน</h2>
       </div>
       <div class="bg-white space-y-4 lg:space-y-6 rounded-2xl px-4 py-4 drop-shadow-md hover:shadow-md active:bg-namjaiyellow">
         <div class="w-[85px] lg:w-1/2 h-[85px] lg:h-[120px] flex mx-auto ">
           <img src="" class="object-cover object-top  w-full" />
         </div>
-        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">การแพทย์</h2>
+        <h2 class="text-namjaidarkgray font-bold text-[14px] lg:text-[18px] text-center tracking-wide">บรรเทาสาธารณภัย</h2>
       </div>
     </div>
     <div class="pt-[40px] lg:pt-[60px] flex justify-center gap-5 lg:gap-10">
-      <button class="px-4 py-2 w-1/2 lg:w-1/6 text-gray-50 text-lg rounded-lg bg-gray-300">ข้าม</button>
+      <button class="px-4 py-2 w-1/2 lg:w-1/6 text-gray-50 text-lg rounded-lg bg-namjaibrown">ภายหลัง</button>
       <button class="px-4 py-2 w-1/2 lg:w-1/6 text-gray-50 text-lg rounded-lg bg-namjaigreen">เสร็จสิ้น</button>
     </div>
   </div>
