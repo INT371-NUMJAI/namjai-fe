6aa113b6fa70fa22ef423f963c67980c19641ae8
fah <supanan.c@mail.kmutt.ac.th>
2022-10-23 21:57:09 +0700

    [edit] w-select in transaction drawer

--- src/components/Transaction/TransactionRequestDrawer.vue
+++ src/components/Transaction/TransactionRequestDrawer.vue
@@ -14,12 +14,14 @@
         <h1 class="text-[25px]">คำร้องถอนเงิน</h1>
         <w-form class="mt-10">
           <h2 class="text-lg">โครงการ</h2>
-          <select name="sort" id="sort" class="mt-5 w-full p-2 text-sm border border-[#9F9F9F] rounded-md">
+          <w-select outline :items="province" color="black" class="h-[40px] mt-5"></w-select>
+
+          <!-- <select name="sort" id="sort" class="mt-5 w-full p-2 text-sm border border-[#9F9F9F] rounded-md">
             <option value="projectName">ชื่อโครงการ</option>
             <option value="status">เพื่อนสูงวัย ไปสุดจี๊ด</option>
             <option value="amount">จำนวนเงิน</option>
             <option value="audi">Audi</option>
-          </select>
+          </select> -->
 
           <div class="text-center flex-wrap justify-center items-center mt-10 space-y-4">
             <h2 class="">จำนวนเงิน</h2>
@@ -41,24 +43,11 @@
 
             <div class="items-center relative">
               <h2 class="text-lg">บัญชี</h2>
-              <button class="w-full mt-5">
-                <div class="bg-white px-2 py-1 flex items-center border border-[#9F9F9F] rounded-md">
-                  <input type="tetx" value="Test" class="py-1 px-2 text-sm appearance-none outline-none w-full text-gray-800 svelte-1l8159u" />
-                  <div @click="showAccount = !showAccount" class="text-gray-300 w-8 flex items-center border-gray-200 svelte-1l8159u">
-                    <button v-if="showAccount == false" class="cursor-pointer rotate-180 w-6 h-6 text-gray-600 outline-none focus:outline-none">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up w-4 h-4">
-                        <polyline points="18 15 12 9 6 15"></polyline>
-                      </svg>
-                    </button>
-                    <button v-if="showAccount" class="cursor-pointer pl-2 w-6 h-6 text-gray-600 outline-none focus:outline-none">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up w-4 h-4">
-                        <polyline points="18 15 12 9 6 15"></polyline>
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-              </button>
-              <div v-if="showAccount" class="absolute z-50 top-100 w-full rounded max-h-select overflow-y-auto shadow">
+              <w-select outline :items="account" color="black" class="h-[40px] mt-5"></w-select>
+              <button class="mt-[20px] w-full text-center text-sm py-2 px-4 bg-white relative hover:bg-gray-300 hover:text-white">
+                  <span @click="showAddAccount = !showAddAccount">เพิ่มบัญชี +</span>
+                </button>
+              <!-- <div v-if="showAccount" class="absolute z-50 top-100 w-full rounded max-h-select overflow-y-auto shadow">
                 <button class="w-full text-left text-sm py-2 px-4 border-b border-slate-300 bg-white relative hover:bg-gray-300 hover:text-white">
                   <span class="">123-456789-0 - ธนาคารไทยพาณิชย์ <br />สมหญิง มั่นคงดุจภูผา</span>
                 </button>
@@ -71,7 +60,7 @@
                 <button class="w-full text-center text-sm py-2 px-4 border-b border-slate-300 bg-white relative hover:bg-gray-300 hover:text-white">
                   <span @click="showAddAccount = !showAddAccount">เพิ่มบัญชี</span>
                 </button>
-              </div>
+              </div> -->
             </div>
           </div>
           <w-form v-if="showAddAccount" class="mt-10">
@@ -104,18 +93,8 @@
   </div>
 </template>
 
-<style scoped>
-.namjaigreen--bg {
-  background-color: #00715d;
-}
-
-.namjaired--bg {
-  background-color: #d45343;
-}
-</style>
-
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 export default {
   setup() {
@@ -123,8 +102,20 @@ export default {
     const showAccount = ref(false);
     const showAddAccount = ref(false);
 
-    // const province = reactive([{ label: "กระบี่" },{ label: "กรุงเทพมหานคร" }, { label: "กาญจนบุรี" }]);
-    return { showDrawer, showAccount, showAddAccount };
+    const province = reactive([{ label: "กระบี่" },{ label: "กรุงเทพมหานคร" }, { label: "กาญจนบุรี" }]);
+
+    const account = reactive([{ label: "123-456789-0 - ธนาคารไทยพาณิชย์ สมหญิง มั่นคงดุจภูผา" },{ label: "123-456789-0 - ธนาคารไทยพาณิชย์ สมหญิง มั่นคงดุจภูผา" },{ label: "123-456789-0 - ธนาคารไทยพาณิชย์ สมหญิง มั่นคงดุจภูผา" }])
+    return { showDrawer, showAccount, showAddAccount, province, account };
   },
 };
 </script>
+
+<style scoped>
+  .namjaigreen--bg {
+    background-color: #00715d;
+  }
+  
+  .namjaired--bg {
+    background-color: #d45343;
+  }
+  </style>
