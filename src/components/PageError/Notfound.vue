5d6f57bf7ec8c6ae15e6f7504eef7f347c0e49cd
fah <supanan.c@mail.kmutt.ac.th>
2022-10-28 11:46:04 +0700

    [feat] api integration

--- src/components/PageError/Notfound.vue
+++ src/components/PageError/Notfound.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="mx-auto mt-80">
-		<h2>NOT FOUND</h2>
+		<h2 class="text-center text-5xl">PAGE NOT FOUND</h2> <br>
+		<p class="text-center text-xl">Page that you are looking for is not exist at this moment.</p>
+		<router-link to="/main">
+		<base-button class="py-2 px-3" buttonLabel="Back to main page"></base-button>
+	</router-link>
 	</div>
 </template>
 
