eff953c9ef40600d2178b62ed25aec30bf5e34fc
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2022-11-03 00:19:34 +0700

    [ADD] Article card and big card

---
+++ src/components/Activity/ArticleBigCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mx-[30px] md:mx-10 lg:mx-44 mt-[120px]">
+    <div class="container mx-auto md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-5 lg:gap-[30px]">
+      <div name="card1" class="max-w-4xl rounded overflow-hidden lg:col-span-2">
+        <img class="w-full h-[205px] md:h-[230px] lg:h-[442px] object-cover rounded-xl" src="@/assets/pic1.png" />
+        <div class="py-5 lg:pt-8 space-y-3">
+          <div class="flex items-center space-x-3 text-[12px] text-zinc-500">
+            <span>มูลนิธิกระจกเงา</span>
+            <hr class="w-2 h-0.5 bg-zinc-500" />
+            <span>01 พ.ย. 65</span>
+          </div>
+          <h2 class="h-[70px] md:h-[85px] overflow-hidden md:text-lg lg:text-2xl lg:leading-normal">สภากาชาดไทยจับมือองค์กรชั้นนำร่วมพาประเทศไทยเข้าสู่นวัตกรรมส่งเสริมความยั่งยืนวงการแพทย์จากการ์ตูน กดเ้กดี้เีกด้เดก้เ</h2>
+        </div>
+      </div>
+      <div class="lg:grid lg:grid-flow-row">
+        <div name="card2" class="hidden md:block max-w-xl rounded overflow-hidden">
+          <img class="w-full h-[205px] md:h-[230px] lg:h-[200px] object-cover rounded-xl" src="@/assets/pic1.png" />
+          <div class="py-5 space-y-3 lg:space-y-1">
+            <div class="flex items-center space-x-3 text-xs text-zinc-500">
+              <span>มูลนิธิกระจกเงา</span>
+              <hr class="w-2 h-0.5 bg-zinc-500" />
+              <span>01 พ.ย. 65</span>
+            </div>
+            <h2 class="h-[70px] md:h-[85px] md:overflow-hidden md:text-lg lg:h-12 overflow-hidden">สภากาชาดไทยจับมือองค์กรชั้นนำร่วมพาประเทศไทยเข้าสู่นวัตกรรมส่งเสริมความยั่งยืนวงการแพทย์จากการ์ตูน กดเ้กดี้เีกด้เดก้เ</h2>
+          </div>
+        </div>
+        <div name="card3" class="hidden lg:block max-w-xl rounded overflow-hidden">
+          <img class="w-full h-[205px] md:h-[230px] lg:h-[200px] object-cover rounded-xl" src="@/assets/pic1.png" />
+          <div class="py-5 space-y-3 lg:space-y-1">
+            <div class="flex items-center space-x-3 text-xs text-zinc-500">
+              <span>มูลนิธิกระจกเงา</span>
+              <hr class="w-2 h-0.5 bg-zinc-500" />
+              <span>01 พ.ย. 65</span>
+            </div>
+            <h2 class="h-[70px] lg:h-12 overflow-hidden">สภากาชาดไทยจับมือองค์กรชั้นนำร่วมพาประเทศไทยเข้าสู่นวัตกรรมส่งเสริมความยั่งยืนวงการแพทย์จากการ์ตูน กดเ้กดี้เีกด้เดก้เ</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Activity/ArticleCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mx-[30px] md:mx-10 lg:mx-44 mt-[120px]">
+    <div class="container mx-auto">
+      <div class="flex space-x-5 md:space-x-8">
+        <img class="w-1/3 lg:w-[25.5%] aspect-square md:aspect-[4/3] object-cover rounded-xl" src="@/assets/blankimg.png" />
+        <div class="space-y-5">
+            <h2 class="border-l-[6px] pl-2 md:pl-4 md:w-11/12 lg:w-8/12 border-namjaiyellow text-[12px] md:text-[18px] lg:text-xl">สภากาชาดไทยจับมือองค์กรชั้นนำร่วมพาประเทศไทยเข้าสู่นวัตกรรมส่งเสริมความยั่งยืนวงการแพทย์จากการ์ตูน กดเ้กดี้เีกด้เดก้เ</h2>
+            <p class="hidden md:block h-24 md:w-11/12 lg:w-8/12 overflow-hidden">จากการ์ตูน ตำรวจสาวป้อมยามตอนสิบ ฉากที่เด็กทารกเสียชีวิตเพราะรถชน แล้วหลุดออกมาจากรถออกมากระแทกพื้นถนนจนร่างกายผิดรูปเสียชีวิตคาที่อันนี้ตามข้อเท็จจริงเลยครับคือไม่ว่าพ่อแม่จะอุ้มลูกดูยังไงก็ตามแต่ถ้า... </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
